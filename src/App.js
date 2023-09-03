249c62bc77c56e077a73b3804c1e55ca7c065d39
Rhyan Lugo Crespo <rhyanlugocrespo@gmail.com>
2023-09-03 18:19:25 -0400

    inputs are more dynamic and added reset button

--- src/App.js
+++ src/App.js
@@ -15,17 +15,33 @@ function Counter() {
   const date = new Date();
   date.setDate(date.getDate() + count);
 
+  function handleReset() {
+    setStep(1);
+    setCount(0);
+  }
+
   return (
     <div>
       <div>
-        <button onClick={() => setStep((prevStep) => prevStep - 1)}>-</button>
-        <span>Step: {step}</span>{" "}
-        <button onClick={() => setStep((prevStep) => prevStep + 1)}>+</button>
+        <input
+          type="range"
+          min={0}
+          max={10}
+          value={step}
+          onChange={(evnt) => setStep(Number(evnt.target.value))}
+        />
+        {/* <button onClick={() => setStep((prevStep) => prevStep - 1)}>-</button> */}
+        <span>{step}</span>
+        {/* <button onClick={() => setStep((prevStep) => prevStep + 1)}>+</button> */}
       </div>
 
       <div>
         <button onClick={() => setCount((prevCount) => prevCount - step)}>-</button>
-        <span>Count: {count}</span>
+        <input
+          type="text"
+          value={count}
+          onChange={(evnt) => setCount(Number(evnt.target.value))}
+        ></input>
         <button onClick={() => setCount((prevCount) => prevCount + step)}>+</button>
       </div>
 
@@ -39,6 +55,12 @@ function Counter() {
         </span>
         <span>{date.toDateString()}</span>
       </p>
+
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
     </div>
   );
 }
